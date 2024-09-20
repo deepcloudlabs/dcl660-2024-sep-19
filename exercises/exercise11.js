d4acfba6f965ce98021310a41bcda07d6d7cc8c2
Binnur KURT <deepcloudlabs@gmail.com>
2024-09-20 15:37:22 +0300

    Update exercise11.js

--- exercises/exercise11.js
+++ exercises/exercise11.js
@@ -1,7 +1,23 @@
+70'li yıllara ait Drama türündeki filmlere, film adına göre sıralı olarak erişmek
+db.movies.find(
+	{
+		$and: [
+		   {"year": {$gte: 1970, $lt: 1980}},
+		   {"genres.name": {$in: ["Drama"]}}
+	    ]
+	}
+).sort({title: 1})
+
 db.movies.aggregate([
 	{
-		$unwind: {
-			"path": "$directors"
-		}	
-	}	
+		$match: {
+			"genres.name" : "Drama",
+			"year": {$gte: 1970, $lt: 1980}
+		}
+	},
+	{
+		$sort: {
+			title: 1
+		}
+	}
 ]);
